6dd9252884ab91e02cb280b98f2b05e09fc22755
voidrlm <varjun609@gmail.com>
2023-11-21 02:30:41 +0530

    Added appbar with title and subtitle

--- grammar-pro/src/App.js
+++ grammar-pro/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <div>
+            <Typography variant="h6" component="div">
+              Grammar Pro
+            </Typography>
+            <Typography variant="subtitle1" color="inherit">
+              Powered by OpenAI
+            </Typography>
+          </div>
+        </Toolbar>
+      </AppBar>
+      {/* The rest of your app content goes here */}
+      <div style={{ padding: "20px" }}>
+        <p></p>
+      </div>
     </div>
   );
 }
